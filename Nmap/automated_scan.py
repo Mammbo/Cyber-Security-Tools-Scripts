417ef2a7187c8d98d1104b703ee7edeee6fafad0
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-05-23 09:19:30 -0500

    updated the function names

--- Nmap/automated_scan.py
+++ Nmap/automated_scan.py
@@ -26,30 +26,30 @@ def makeOwner(path):
 
 
 #host discovery 
-def icmp_echo_host_discovery(target, out_xml, formatted_time):
+def icmp_echo_host_scan(target, out_xml, formatted_time):
     out_xml = os.path.join(out_xml, f"{formatted_time}icmp_echo_host_discovery.xml")
     nmap_command = f"/usr/bin/nmap {target} -n -sn -PE -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
     subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
     makeOwner(out_xml)
  
-def icmp_netmask_host_discovery(target, out_xml, formatted_time):
+def icmp_netmask_host_scan(target, out_xml, formatted_time):
     out_xml = os.path.join(out_xml, f"{formatted_time}icmp_netmask_host_discovery.xml")
-    nmap_command = f"/usr/bin/nmap {target} -n -sn -PE -vv -oX {out_xml}"
+    nmap_command = f"/usr/bin/nmap {target} -n -sn -PM -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
     subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
     makeOwner(out_xml)
 
-def icmp_timestamp_host_discovery(target, out_xml, formatted_time):
+def icmp_timestamp_host_scan(target, out_xml, formatted_time):
     out_xml = os.path.join(out_xml, f"{formatted_time}icmp_timestamp_host_discovery.xml")
-    nmap_command = f"/usr/bin/nmap {target} -n -sn -PE -vv -oX {out_xml}"
+    nmap_command = f"/usr/bin/nmap {target} -n -sn -PP -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
     subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
     makeOwner(out_xml)
 
-def tcp_syn_host_discovery(target, out_xml, formatted_time):
+def tcp_syn_host_scan(target, out_xml, formatted_time):
     out_xml = os.path.join(out_xml, f"{formatted_time}tcp_syn_host_discovery.xml")
-    nmap_command = f"/usr/bin/nmap {target} -n -sn -PE -vv -oX {out_xml}"
+    nmap_command = f"/usr/bin/nmap {target} -PS21,22,23,25,80,113,443 -PA80,113,443 -n -sn -T4 -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
     subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
     makeOwner(out_xml)
@@ -85,13 +85,11 @@ def main():
     
     target = '192.'
 
-    icmp_echo_host_discovery(target, os.getcwd)
-    icmp_netmask_host_discovery(target, os.getcwd)
-    icmp_timestamp_host_discovery(target, os.getcwd)
-    tcp_syn_host_discovery(target, os.getcwd)
+    icmp_echo_host_scan(target, os.getcwd())
+    icmp_netmask_host_scan(target, os.getcwd())
+    icmp_timestamp_host_scan(target, os.getcwd())
+    tcp_syn_host_scan(target, os.getcwd())
 
-
-
-
-main()
+if __name__ == '__main__':
+    main()
 
