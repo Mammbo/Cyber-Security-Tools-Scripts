82890059df7a0f83c2b7b850f11df671600a827a
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-05-24 12:25:52 -0500

    the automated scan which will be the basis of all the other files now works so I should be good now to continue with the database and csv parser of this project.

--- Nmap/automated_scan.py
+++ Nmap/automated_scan.py
@@ -8,7 +8,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime 
 
 time = datetime.now()
-formatted_time = time.strftime("%Y/%m/%d_%H-%M")
+formatted_time = time.strftime("%Y-%m-%d_%H-%M")
 
 # root check 
 def root(): 
@@ -61,53 +61,59 @@ def parseDiscoverPorts(in_xml):
 
 #host discovery 
 def icmp_echo_host_scan(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml, f"{formatted_time}icmp_echo_host_discovery.xml")
+    out_xml = os.path.join(out_xml, f"{formatted_time}--icmp_echo_host_discovery.xml")
     nmap_command = f"/usr/bin/nmap {target} -n -sn -PE -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
-    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
+    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     makeOwner(out_xml)
+    return out_xml 
  
 def icmp_netmask_host_scan(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml, f"{formatted_time}icmp_netmask_host_discovery.xml")
+    out_xml = os.path.join(out_xml, f"{formatted_time}--icmp_netmask_host_discovery.xml")
     nmap_command = f"/usr/bin/nmap {target} -n -sn -PM -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
-    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
+    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     makeOwner(out_xml)
+    return out_xml
 
 def icmp_timestamp_host_scan(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml, f"{formatted_time}icmp_timestamp_host_discovery.xml")
+    out_xml = os.path.join(out_xml, f"{formatted_time}--icmp_timestamp_host_discovery.xml")
     nmap_command = f"/usr/bin/nmap {target} -n -sn -PP -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
-    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
+    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     makeOwner(out_xml)
+    return out_xml
 
 def tcp_syn_host_scan(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml, f"{formatted_time}tcp_syn_host_discovery.xml")
+    out_xml = os.path.join(out_xml, f"{formatted_time}--tcp_syn_host_discovery.xml")
     nmap_command = f"/usr/bin/nmap {target} -PS21,22,23,25,80,113,443 -PA80,113,443 -n -sn -T4 -vv -oX {out_xml}"
     nmap_args = shlex.split(nmap_command)
-    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE.communicate())
+    subprocess.Popen(nmap_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     makeOwner(out_xml)
+    return out_xml
 
 #port scanning (1000)
 
 def tcpsyn_port_scan_1000(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml,f'{formatted_time}top_1000_portscan.xml')
+    out_xml = os.path.join(out_xml,f'{formatted_time}--top_1000_portscan.xml')
     nmap_cmd = f"/usr/bin/nmap {target} --top-ports 1000 -n -Pn -sS -T4 --min-parallelism 100 --min-rate 64 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     makeOwner(out_xml)
+    return out_xml 
 
 #service detection 
 def service_scan(target_ip, target_ports, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml,f'{formatted_time}-{target_ip}_services.xml')
+    out_xml = os.path.join(out_xml,f'{formatted_time}--{target_ip}_services.xml')
     nmap_cmd = f"/usr/bin/nmap {target_ip} -p {target_ports} --exclude-ports 9100-9107, 515, 1028, 1068, 1503, 1720, 1935, 2040, 3388 -n -Pn -sV --version-intensity 6 --script banner -T4 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    makeOwner(out_xml)
 
 #os detection 
 
 def os_scan(targets, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml,f'{formatted_time}osdetection.xml')
+    out_xml = os.path.join(out_xml,f'{formatted_time}--osdetection.xml')
     nmap_cmd = f"/usr/bin/nmap {targets} -n -Pn -O -T4 --min-parallelism 100 --min-rate 64 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -120,6 +126,7 @@ def ssl_cipher_scan(target_ip, target_ports, out_xml, formatted_time):
     nmap_cmd = f"/usr/bin/nmap {target_ip} -p {target_ports} -n -Pn --script ssl-enum-ciphers -T4 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    makeOwner(out_xml)
 
 
 #ssl certs 
@@ -128,10 +135,11 @@ def ssl_cert_scan(target_ip, target_ports, out_xml, formatted_time):
     nmap_cmd = f"/usr/bin/nmap {target_ip} -p {target_ports} -n -Pn --script ssl-cert -T4 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    makeOwner(out_xml)
 #all ports scan 
 
 def tcp_syn_port_scan_all(target, out_xml, formatted_time):
-    out_xml = os.path.join(out_xml,f'{formatted_time}65535_portscan.xml')
+    out_xml = os.path.join(out_xml,f'{formatted_time}--65535_portscan.xml')
     nmap_cmd = f"/usr/bin/nmap {target} -p- -n -Pn -sS -T4 --min-parallelism 100 --min-rate 128 -vv -oX {out_xml}"
     sub_args = shlex.split(nmap_cmd)
     subprocess.Popen(sub_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -148,27 +156,28 @@ def main():
     
     target = ''
 
-    icmp_echo_host_scan(target, os.getcwd())
-    icmp_netmask_host_scan(target, os.getcwd())
-    icmp_timestamp_host_scan(target, os.getcwd())
-    tcp_syn_host_scan(target, os.getcwd())
+    scan1 = icmp_echo_host_scan(target, os.getcwd(), formatted_time)
+    scan2 = icmp_netmask_host_scan(target, os.getcwd(), formatted_time)
+    scan3 = icmp_timestamp_host_scan(target, os.getcwd(), formatted_time)
+    scan4 = tcp_syn_host_scan(target, os.getcwd(), formatted_time)
 
 
-    hosts = parseDiscoverXml('any dir')
-    hosts += parseDiscoverXml('any dir')
-    hosts += parseDiscoverXml('any dir')
-    hosts += parseDiscoverXml('any dir')
+    hosts = parseDiscoverXml(scan1)
+    hosts += parseDiscoverXml(scan2)
+    hosts += parseDiscoverXml(scan3)
+    hosts += parseDiscoverXml(scan4)
 
     target = convertToNmapTarget(hosts)
+    #this function is here so when the 1000 port scan is executed its output can be used for the parse the discovered ports function
+    location_of_port_scan = tcpsyn_port_scan_1000(target, os.getcwd(), formatted_time)
 
-    targets = parseDiscoverPorts('anydir/top_1000_portscan.xml')
+    targets = parseDiscoverPorts(location_of_port_scan)
     for target in targets:
         element = target.split()
         target_ip = element[0]
         target_ports = element[1]
-        print(f'Nmap Format Example: nmap {target_ip} -p {target_ports}')
-
-    tcpsyn_port_scan_1000(target, os.getcwd(), formatted_time)
+   
+   #stage 2 scans after we get the important information to conduct these scans 
     service_scan(target_ip, target_ports, os.getcwd(), formatted_time)
     os_scan(targets, os.getcwd(), formatted_time)
     ssl_cipher_scan(target_ip, target_ports, os.getcwd(), formatted_time)
